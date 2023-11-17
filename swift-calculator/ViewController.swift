a035d817a1c6df33cf5ce4530bd39fc097202dda
olzhaskhamzin <olzhas.khamzin@gmail.com>
2023-11-17 13:02:50 +0600

    adding multiply, divide, and divide with remainder methods

--- swift-calculator/ViewController.swift
+++ swift-calculator/ViewController.swift
@@ -21,32 +21,47 @@ class ViewController: UIViewController {
 
     
     @IBAction func plus(_ sender: Any) {
-        var num1 = Int(leftOperand.text!) ?? 0
-        var num2 = Int(rightOperand.text!) ?? 0
-        var res = num1 + num2
+        let num1 = Int(leftOperand.text!) ?? 0
+        let num2 = Int(rightOperand.text!) ?? 0
+        let res = num1 + num2
         
         result.text = String(res)
     }
     
 
     @IBAction func minus(_ sender: Any) {
-        var num1 = Int(leftOperand.text!) ?? 0
-        var num2 = Int(rightOperand.text!) ?? 0
-        var res = num1 - num2
+        let num1 = Int(leftOperand.text!) ?? 0
+        let num2 = Int(rightOperand.text!) ?? 0
+        let res = num1 - num2
         
         result.text = String(res)
     }
     
     
     @IBAction func multiply(_ sender: Any) {
+        let num1 = Int(leftOperand.text!) ?? 1
+        let num2 = Int(rightOperand.text!) ?? 1
+        let res = num1 * num2
+        
+        result.text = String(res)
     }
     
     
     @IBAction func divide(_ sender: Any) {
+        let num1 = Double(leftOperand.text!) ?? 1
+        let num2 = Double(rightOperand.text!) ?? 1
+        let res = num1 / num2
+        
+        result.text = String(res)
     }
     
     
     @IBAction func divideWithRemainder(_ sender: Any) {
+        let num1 = Double(leftOperand.text!) ?? 1
+        let num2 = Double(rightOperand.text!) ?? 1
+        let res: Double = num1.truncatingRemainder(dividingBy: num2)
+        
+        result.text = String(res)
     }
     
     
