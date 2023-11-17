70e557af708f4372a5fadc53ecc63ce19749e04a
olzhaskhamzin <olzhas.khamzin@gmail.com>
2023-11-17 12:51:42 +0600

    adding a plus method

--- swift-calculator/ViewController.swift
+++ swift-calculator/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var leftOperand: UITextField!
+    @IBOutlet weak var rightOperand: UITextField!
+    @IBOutlet weak var result: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func plus(_ sender: Any) {
+        var num1 = Int(leftOperand.text!) ?? 0
+        var num2 = Int(rightOperand.text!) ?? 0
+        var res = num1 + num2
+        
+        result.text = String(res)
+    }
+    
 
+    @IBAction func minus(_ sender: Any) {
+    }
+    
+    
+    @IBAction func multiply(_ sender: Any) {
+    }
+    
+    
+    @IBAction func divide(_ sender: Any) {
+    }
+    
+    
+    @IBAction func divideWithRemainder(_ sender: Any) {
+    }
 }
 
